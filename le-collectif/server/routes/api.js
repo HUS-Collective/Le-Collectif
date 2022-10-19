3b60e14a055c9e63683e7965b7485abd0cc1271a
Yuehao Wong <yuehaowong@gmail.com>
2022-10-18 21:48:59 -0400

    createdroute for posting images to db

--- le-collectif/server/routes/api.js
+++ le-collectif/server/routes/api.js
@@ -2,15 +2,9 @@ const express = require("express");
 const Router = express.Router();
 const collectiveController = require("../controllers/collectiveController");
 
-//Get Request
-
 // Post Request  ::: LOGIN/SIGN UP
 Router.post("/", collectiveController.createUser, (req, res) => {
   return res.status(200).json(res.locals.user);
 });
-
-// Delete Request
-
-// Put Request
 
 module.exports = Router;
